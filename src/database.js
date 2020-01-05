be6c7aa0a7769b60650d539ea5260a61665f9ffa
Johan <johan225@hotmail.es>
2020-01-05 10:57:41 -0500

    Modify rutes

--- src/database.js
+++ src/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 const { mongodb } = require('./keys')
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI || mongodb.URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(db => console.log('Database is connected'))
   .catch(err => console.error(err))
