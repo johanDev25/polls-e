f0e7e48131740f1872b7efb7d881e4e4b10f792a
Johan <johan225@hotmail.es>
2020-01-05 09:46:40 -0500

    Modify rutes

--- src/passport/local-auth.js
+++ src/passport/local-auth.js
@@ -1,7 +1,7 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 
-const User = require('./src/models/user');
+const User = require('./models/user');
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
